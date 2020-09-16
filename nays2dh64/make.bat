313444f7fa330d74eb1fad601aa729d132d0a44d
Keisuke Inoue <keisuke.inoue@mizuho-ir.co.jp>
2020-09-16 18:58:53 +0900

    iricmi 対応を開始

--- nays2dh64/make.bat
+++ nays2dh64/make.bat
@@ -1,8 +1,6 @@
-ifort 	..\libs\cgnsdll.lib^
-	..\libs\iriclib.lib ^
-	..\src\Nays2DH.f90 /Qopenmp /nostandard-realloc-lhs /MD -o nays2dh.exe
-	
-
+ifort /MD /c /Qopenmp /O3 ../src/iricmi.f90
+ifort /MD /c /Qopenmp /O3 ../src/Nays2DH.f90
+ifort *.obj ../libs/cgnsdll.lib ../libs/iriclib.lib ../libs/iricmi_f.lib -o nays2dh.exe
 del *.obj 
 del *.mod 
 
