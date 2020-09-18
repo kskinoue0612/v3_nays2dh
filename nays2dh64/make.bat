52f03d28b06a5afa8258e65566a8896e81004bc1
Keisuke Inoue <keisuke.inoue@mizuho-ir.co.jp>
2020-09-18 18:29:20 +0900

    iriclib, cgnslib へのリンクを削除

--- nays2dh64/make.bat
+++ nays2dh64/make.bat
@@ -1,6 +1,6 @@
 ifort /MD /c /Qopenmp /O3 ../src/iricmi.f90
 ifort /MD /c /Qopenmp /O3 ../src/Nays2DH.f90
-ifort *.obj ../libs/cgnsdll.lib ../libs/iriclib.lib ../libs/iricmi_f.lib -o nays2dh.exe
+ifort *.obj ../libs/iricmi_f.lib -o nays2dh.exe
 del *.obj 
 del *.mod 
 
