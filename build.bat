801ba8c26f117bb4273fed2772860e26c7c9d7a8
Keisuke Inoue <kskinoue@gmail.com>
2021-09-13 15:29:57 +0900

    Modified so that files to install are copyed to INSTALL folder.

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2019
 
-mkdir ..\INSTALL
-mkdir ..\INSTALL\bin
+mkdir INSTALL
 
 rem build nays2dh.exe
 
@@ -9,12 +8,12 @@ ifort src\iric.f90 /Qopenmp /nostandard-realloc-lhs /MD /c
 ifort src\Nays2DH.f90 /Qopenmp /nostandard-realloc-lhs /MD /c
 ifort *.obj libs\iriclib.lib -o nays2dh.exe
 
-rem copy nays2dh.exe to bin folder
+rem copy nays2dh.exe to INSTALL folder
 
-copy nays2dh.exe ..\INSTALL\bin\nays2dh.exe
+copy nays2dh.exe INSTALL\nays2dh.exe
 
 rem copy files to bundle to installer
 rem ---------------------------------
 
-copy nays2dh64\definition.xml ..\INSTALL\bin\definition.xml
-copy nays2dh64\translation_ja_JP.ts ..\INSTALL\bin\translation_ja_JP.ts
+copy nays2dh64\definition.xml INSTALL\definition.xml
+copy nays2dh64\translation_ja_JP.ts INSTALL\translation_ja_JP.ts
